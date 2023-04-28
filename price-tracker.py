19a5cd5d0270e79dfabdac8713ada9b6bec99481
alright21 <alberto.giust21@gmail.com>
2023-04-28 13:49:16 +0200

    add: initial commit

---
+++ price-tracker.py
@@ -0,0 +1,11 @@
+
+'''
+Project buillt starting from https://oxylabs.io/blog/how-to-build-a-price-tracker
+'''
+
+import smtplib
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from price_parser import Price
+
